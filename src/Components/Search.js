43d3a1449a24fc58c0e946b1ae4a0e6d39c87b55
Ganesh <id.ganesh123@gmail.com>
2018-05-19 02:33:39 +0530

    searching api

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 
+import Artist from './Artist';
+
 export default class Search extends Component {
     constructor(props){
         super(props)
         this.state = {
             txt:'',
-            artist:[]
+            artist: []
         }
     }
     componentDidMount(){
@@ -16,25 +18,33 @@ export default class Search extends Component {
 
     }
     txtF(event) {
+      // console.log(event);
         this.setState({
             txt : event.target.value
         })
         axios.get("http://www.theaudiodb.com/api/v1/json/1/search.php?s="+event.target.value)
         .then((resp,req)=>{
+          console.log(resp.data.artists);
             this.setState({
             artist: resp.data.artists
+            // artist: []
             })
-            console.log(this.state.artist)
+
+
         })
         .catch(console.error)
+        console.log(this.state.artist);
     }
 
     render() {
         return (
         <div id="search">
-            <input placeholder="Search" className = 'searchField' type="text" value={this.state.txt} onChange={(e) => this.txtF(e)} />
-            {this.state.txt}
-
+            <input placeholder="Search" className = 'searchField' type="text" value={this.state.txt} onChange={(e) => this.txtF(e)} align="middle"/>
+            <table border="0" align="right"><tbody><tr>
+                <td><img class="searchImg" height="90" src={this.state.artist?this.state.artist[0] !== undefined ? this.state.artist[0].strArtistThumb:"":""} align="middle"/></td>
+                <td><img class="searchImg" height="90" src={this.state.artist?this.state.artist[1] !== undefined ? this.state.artist[1].strArtistThumb:"":""} align="middle"/></td>
+                <td><img class="searchImg" height="90" src={this.state.artist?this.state.artist[2] !== undefined ? this.state.artist[2].strArtistThumb:"":""} align="middle"/></td>
+            </tr></tbody></table>
         </div>
         );
     }
